45a2e3ad93a9e7fd7566ce7ac7b3493e4596a661
Vinob <vinobchanderr@gmail.com>
2024-06-28 06:52:58 +0530

    update test and jenkins file

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,23 +1,9 @@
 const request = require("supertest");
-const { app } = require("../app");
-
-let server;
-
-beforeAll((done) => {
-  const PORT = process.env.TEST_PORT || 3001;
-  server = app.listen(PORT, () => {
-    console.log(`Test server is running on port ${PORT}`);
-    done();
-  });
-});
-
-afterAll((done) => {
-  server.close(done);
-});
+const app = require("../app");
 
 describe("GET /", () => {
   it("should return Hello, Jenkins CI/CD with Node.js!", (done) => {
-    request(`http://localhost:${process.env.TEST_PORT || 3001}`)
+    request(app)
       .get("/")
       .expect(200)
       .expect("Hello, Jenkins CI/CD with Node.js!", done);
