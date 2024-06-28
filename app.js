6ccbddb846d685f1048b2ec67a70f0407aa4cf75
Vinob <vinobchanderr@gmail.com>
2024-06-28 05:46:43 +0530

    Added about route to demonstrate Jenkins CI/CD

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get("/", (req, res) => {
   res.send("Hello, Jenkins CI/CD with Node.js!");
 });
 
+// New route added
+app.get("/about", (req, res) => {
+  res.send("About Page: Jenkins CI/CD with Node.js!");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
