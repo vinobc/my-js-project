94633c9d1c471085df2420be4087d339d1613334
Vinob <vinobchanderr@gmail.com>
2024-06-28 12:42:16 +0530

    change app.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -17,7 +17,7 @@ afterAll((done) => {
 });
 
 describe("GET /", () => {
-  it("should return Hello, Jenkins CI/CD with Node.js!", (done) => {
+  it("should return 200", (done) => {
     request(`http://localhost:${process.env.PORT}`)
       .get("/")
       .expect(200)
